7074d0b32d5f9e89e295491d427582bf37f740aa
Vincent Rousseau <vincent.rousseau@irstea.fr>
2017-07-28 10:35:19 +0200

    Add 3 working test case

--- test/src/urdf_geometry_parser_test.cpp
+++ test/src/urdf_geometry_parser_test.cpp
@@ -1,27 +1,47 @@
 #include <ros/ros.h>
 #include <urdf_geometry_parser/urdf_geometry_parser.h>
 
+#include <gtest/gtest.h>
+
+class UrdfGeometryParserTest : public ::testing::Test{
+public:
+  UrdfGeometryParserTest() :
+    ugp_(nh_, "base_link")
+  {}
+
+  ros::NodeHandle nh_;
+  urdf_geometry_parser::UrdfGeometryParser ugp_;
+
+};
+
 TEST_F(UrdfGeometryParserTest, testRadius)
 {
-  ros::NodeHandle nh;
-  std::string base_frame_id = "base_link";
-  float wheel_radius;
-
-  urdf_geometry_parser::UrdfGeometryParser uvk(nh, base_frame_id);
-
-//    if(!uvk.getDistanceBetweenJoints(front_steering_names[0], front_steering_names[1], track_))
-//      return false;
-//    else
-//      nh.setParam("track",track_);
-
-  bool result = uvk.getJointRadius("front_left_wheel", wheel_radius);
-
-//    if(!uvk.getDistanceBetweenJoints(front_wheel_names[0], rear_wheel_names[0], wheel_base_))
-//      return false;
-//    else
-//      nh.setParam("wheel_base",wheel_base_);
+  double wheel_radius;
+  bool result = ugp_.getJointRadius("front_left_wheel", wheel_radius);
+  EXPECT_EQ(result, true);
+  EXPECT_EQ(wheel_radius, 0.28);
 }
 
+TEST_F(UrdfGeometryParserTest, testDistance)
+{
+  double track, wheel_base;
+
+  bool result_track = ugp_.getDistanceBetweenJoints("front_left_wheel", "front_right_wheel", track);
+  EXPECT_EQ(result_track, true);
+  EXPECT_EQ(track, 1.1);
+
+  bool result_wb = ugp_.getDistanceBetweenJoints("front_right_wheel", "rear_right_wheel", wheel_base);
+  EXPECT_EQ(result_wb, true);
+  EXPECT_EQ(wheel_base, 1.9);
+}
+
+TEST_F(UrdfGeometryParserTest, testJointSteeringLimits)
+{
+  double steering_limit;
+  bool result = ugp_.getJointSteeringLimits("rear_left_steering_joint", steering_limit);
+  EXPECT_EQ(result, true);
+  EXPECT_NEAR(steering_limit, M_PI/6, 0.001);
+}
 
 int main(int argc, char** argv)
 {
